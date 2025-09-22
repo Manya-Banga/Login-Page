4db58ddada4996a3ecfe21d0c25f841892f27384
Manya-Banga <manyabanga769@gmail.com>
2025-09-22 20:17:35 +0530

    fixed Views path in  index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.set("view engine", "ejs");
 
 // set views folder explicitly (optional but good practice)
-app.set("views", path.join(__dirname, "views"));
+app.set("Views", path.join(__dirname, "Views"));
 
 // serve static files (css, js, images) from public folder
 app.use(express.static(path.join(__dirname, "public")));
@@ -19,6 +19,21 @@ const PORT = process.env.PORT || 8000;
 app.get("/", (req, res) => {
   res.render("index"); // looks for views/index.ejs
 });
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/services", (req, res) => {
+  res.render("services");
+});
+
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+
 
 app.listen(PORT, () => {
  console.log(`Server running on http://localhost:${PORT}`);
